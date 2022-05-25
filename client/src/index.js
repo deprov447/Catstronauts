70e436749fcbd9c60d0b184d70158fb8cf266b40
Anubhav Choudhary <ac.10edu@gmail.com>
2022-05-25 15:38:03 +0530

    ENV vars changed
    Signed-off-by: Anubhav Choudhary <ac.10edu@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,10 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const API_END = process.env.REACT_APP_API_ENDPOINT
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: API_END,
   cache: new InMemoryCache(),
 });
 
